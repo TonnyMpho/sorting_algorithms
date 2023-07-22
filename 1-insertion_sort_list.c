f896daf3b28ee37bce8acdea92bdb797dbd49af9
TonnyMpho <tonnympho1@gmail.com>
2023-07-22 14:52:30 -0700

    0x1B. C - Sorting algorithms & Big O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,13 +10,12 @@
  */
 void swap_nodes(listint_t **list, listint_t *node_1, listint_t *node_2)
 {
+	if (node_2->next != NULL)
+		node_2->next->prev = node_1;
 	if (node_1->prev)
 		node_1->prev->next = node_2;
 	else
 		*list = node_2;
-
-	if (node_2->next != NULL)
-		node_2->next->prev = node_1;
 
 	node_1->next = node_2->next;
 	node_2->prev = node_1->prev;
@@ -50,7 +49,6 @@ void insertion_sort_list(listint_t **list)
 		while (curr_node->prev && curr_node->prev->n > next_node->n)
 		{
 			swap_nodes(list, curr_node->prev, curr_node);
-			curr_node = curr_node->prev;
 			print_list(*list);
 		}
 		next_node = next_node->next;
