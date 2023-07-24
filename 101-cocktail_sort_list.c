9a2f29895bd393dc0e1461ffc92d8265d11b7c6f
TonnyMpho <tonnympho1@gmail.com>
2023-07-24 07:57:40 -0700

    0x1B. C - Sorting algorithms & Big O

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -39,7 +39,7 @@ void cocktail_sort_list(listint_t **list)
 	listint_t *head = NULL, *curr_node, *end = NULL;
 	int swap = 0;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (swap == 0)
